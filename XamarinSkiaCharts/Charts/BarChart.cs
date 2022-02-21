2ce983132ca0c60737f003e064514ff4c5d4f34d
Jon <nichojo89@gmail.com>
2022-02-21 13:45:59 -0500

    Added working bar chart

---
+++ XamarinSkiaCharts/Charts/BarChart.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace XamarinSkiaCharts.Charts
+{
+    public class BarChart : SKCanvasView
+    {
+        public static readonly BindableProperty LoadChartProperty = BindableProperty.Create(nameof(LoadChart),
+            typeof(bool),
+            typeof(BarChart),
+            false,
+            propertyChanged: async (bindable, oldValue, newValue) =>
+            {
+                if ((bool)newValue)
+                    await ((BarChart)bindable).LoadChartAnimation();
+            });
+
+        public bool LoadChart
+        {
+            get => (bool)GetValue(LoadChartProperty);
+            set => SetValue(LoadChartProperty, value);
+        }
+
+        public static readonly BindableProperty PointsProperty = BindableProperty.Create(nameof(Points),
+            typeof(List<int>),
+            typeof(BarChart),
+            new List<int>(),
+            propertyChanged: async (bindable, oldValue, newValue) =>
+            {
+                var chart = ((BarChart)bindable);
+                if (!chart.ChartsLoading)
+                {
+                    //New data added, re-render chart without loading animation
+                    ((BarChart)bindable).InvalidateSurface();
+                }
+            });
+
+        public List<int> Points
+        {
+            get => (List<int>)GetValue(PointsProperty);
+            set => SetValue(PointsProperty, value);
+        }
+
+        public BarChart()
+        {
+            VerticalOptions = LayoutOptions.FillAndExpand;
+            EnableTouchEvents = true;
+        }
+
+        protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
+        {
+            base.OnPaintSurface(e);
+
+            var info = e.Info;
+            var canvas = e.Surface.Canvas;
+            canvas.Clear();
+            _chartWidth = info.Width;
+            const int BAR_WIDTH = 100;
+
+            if (_moved)
+            {
+                _firstBarXAxis += _xMoved;
+            }
+            var barXAxis = _firstBarXAxis;
+            
+            using (var paint = new SKPaint())
+            {
+                // Create gradient for background
+                var transparentMauiPurpleColor = new SKColor(0XB2, 0X7F, 0XFF, 0X0);
+                var mauiPurpleColor = new SKColor(0XB2, 0X7F, 0XFF);
+                paint.Shader = SKShader.CreateLinearGradient(
+                                    new SKPoint(0, 0),
+                                    new SKPoint(info.Width, info.Height),
+                                    new SKColor[] { mauiPurpleColor, transparentMauiPurpleColor },
+                                    null,
+                                    SKShaderTileMode.Clamp);
+
+                for (var i = 0; i < Points.Count; i++)
+                {
+                    var barHeight = info.Height - (info.Height * (Points[i] / _max) * _barScale);
+                    var bar = new SKRect(barXAxis, barHeight, barXAxis + BAR_WIDTH, info.Height);
+
+                    canvas.DrawRect(bar, paint);
+                    barXAxis += BAR_WIDTH + 20;
+
+                    if (i == Points.Count - 1)
+                        _lastBarXAxis = barXAxis + BAR_WIDTH;
+                }
+
+            }
+        }
+
+        protected override void OnTouch(SKTouchEventArgs e)
+        {
+            base.OnTouch(e);
+            if (!ChartsLoading)
+            {
+                switch (e.ActionType)
+                {
+                    case SKTouchAction.Pressed:
+                        _moved = true;
+                        _xOrigin = e.Location.X;
+                        break;
+                    default:
+                        var scrolled = (_xOrigin - e.Location.X) * -1;
+                        var scrolledToLeftChartEdge = _firstBarXAxis + scrolled >= 40;
+                        var scrolledToRightChartEdge = _lastBarXAxis + scrolled <= _chartWidth;
+                        if (scrolledToLeftChartEdge || scrolledToRightChartEdge)
+                            return;
+
+                        _xMoved = (_xOrigin - e.Location.X) * -1;
+                        _xOrigin = e.Location.X;
+                        InvalidateSurface();
+
+                        break;
+                }
+            }
+            e.Handled = true;
+        }
+
+        /// <summary>
+        /// Animates bars from 1/30 of their height to 30/30
+        /// loop starts at 29 because the first canvas load han
+        /// </summary>
+        public async Task LoadChartAnimation()
+        {
+            for (var i = 0; i <= 30; i++)
+            {
+                _barScale = i / 30f;
+                InvalidateSurface();
+                await Task.Delay(33);
+            }
+            ChartsLoading = false;
+        }
+
+
+        private float _xOrigin;
+        private float _max = 10;
+        private float _chartWidth;
+        private float _xMoved = -1;
+        private float _lastBarXAxis;
+        private bool _moved = false;
+        private float _barScale = 0.0f;
+        public bool ChartsLoading = true;
+        private float _firstBarXAxis = 20.0f;
+       
+    }
+}
---
+++ XamarinSkiaCharts/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace XamarinSkiaCharts
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new List<int>()
+            {
+                3,7,3,6,4,9,3,7,5,1,6,4,7
+            };
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            BarChart.LoadChart = true;
+        }
+    }
+}
